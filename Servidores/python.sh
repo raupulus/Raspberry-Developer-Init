f16ce4a986085d579b05c66e51fd6f151f2df10b
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-05-30 12:42:29 +0200

    Añado instalación y dependencias de python

--- Servidores/python.sh
+++ Servidores/python.sh
@@ -16,13 +16,20 @@
 ############################
 ##     INSTRUCCIONES      ##
 ############################
+## Instala python 2, python 3, Django y gestor de paquete pip para ambas
+## versiones de php.
+## Además instala paquetes básicos recurridos, correctores de sintaxis y reglas
+## de estilos para PEP8 principalmente.
 
 ############################
 ##        FUNCIONES       ##
 ############################
 
 python_descargar() {
-    echo "$VE Descargando$RO python$CL"
+    echo -e "$VE Descargando$RO python$CL"
+
+    ## Descargando pyenv para gestionar versión de python en un proyecto
+    descargarGIT 'Pyenv' 'https://github.com/pyenv/pyenv.git' "$HOME/.pyenv"
 }
 
 python_preconfiguracion() {
@@ -33,6 +40,12 @@ python_instalar() {
     echo -e "$VE Instalando$RO Python y Django$CL"
     ## Instalar python y gestor de paquetes
     instalarSoftware python python3 python-pip python3-pip
+
+    ## Instalando dependencias
+    echo -e "$VE Instalando dependencias$CL"
+
+    local dependencias='flake8 pyflakes pyflakes3 pydocstyle pylama prospector virtualenv'
+    instalarSoftware "$dependencias"
 }
 
 python_postconfiguracion() {
@@ -40,10 +53,20 @@ python_postconfiguracion() {
 
     configurar_python2() {
         echo -e "$VE Preparando configuracion de$RO Python2$CL"
+        echo -e "$VE Instalando dependencias para Python 2$CL"
+
+        local dependencias='python-autopep8 python-bottle python-cryptography python-dev python-enum34 python-flake python-frozendict python-future python-idna python-ipaddress python-ipython python-jedi python-mccabe python-mysqldb python-openssl python-pep8 python-pip python-powerline python-powerline-taskwarrior python-pyasn1 python-pycodestyle python-pyflakes python-pygments python-pylama python-setuptools python-urllib3 python-virtualenv python-waitress'
+
+        instalarSoftware "$dependencias"
     }
 
     configurar_python3() {
         echo -e "$VE Preparando configuracion de$RO Python3$CL"
+        echo -e "$VE Instalando dependencias para Python 3$CL"
+
+        local dependencias='python3-dev python3-flake8 python3-frozendict python3-future python3-ipython python3-mccabe python3-mysqldb python3-pep8 python3-pep8-naming python3-pip python3-pylama python3-powerline python3-powerline-taskwarrior python3-pycodestyle python3-pyflakes python3-setuptools python3-virtualenv'
+
+        instalarSoftware "$dependencias"
     }
 
     personalizar_python2() {
