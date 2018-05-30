c0ea1d52410ed439050975c1bb37c6401ce0f27a
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-05-30 12:36:39 +0200

    Añado aplicaciones extras

--- Apps/3_Aplicaciones_Usuario.sh
+++ Apps/3_Aplicaciones_Usuario.sh
@@ -15,19 +15,90 @@
 ############################
 ##     INSTRUCCIONES      ##
 ############################
+## Instala software que se configura para cada usuario de forma independiente
+## y necesitará ser ejecutado una vez por cada usuario que quiera implementar
+## estas funcionalidades para si mismo.
 
 ############################
 ##     IMPORTACIONES      ##
 ############################
+source "$WORKSCRIPT/Apps/bashit.sh"
+source "$WORKSCRIPT/Apps/OhMyZsh.sh"
+source "$WORKSCRIPT/Apps/spacevim.sh"
+source "$WORKSCRIPT/Apps/vim.sh"
 
 ############################
 ##       FUNCIONES        ##
 ############################
+configurar_heroku() {
+    echo -e "$VE Se va a configurar$RO Heroku$CL"
+    instalarSoftware 'heroku'
+    echo -e "$VE ¿Quieres configurar tu cuenta de$RO Heroku?$CL"
+    echo -e "$VE Para configurar la cuenta tienes que tenerla creada$CL"
+    read -p '    s/N → ' input
+    if [[ $input = 's' ]] || [[ $input = 'S' ]]; then
+        heroku login
+    fi
+}
+
+##
+## Mi generador de proyectos https://github.com/fryntiz/Generador_Proyectos.git
+## Este generador de proyectos crea un script que permite generar la estructura
+## para los proyectos más recurridos por mi (php, python, bash....) y después
+## pregunta si subirlo automáticamente a GitHub
+##
+generador_proyectos() {
+    descargarGIT 'Generador de Proyectos' 'https://github.com/fryntiz/Generador_Proyectos.git' "$WORKSCRIPT/tmp/Generador_Proyectos"
+
+    cd "$WORKSCRIPT/tmp/Generador_Proyectos" || return 1 && ./instalar.sh
+    cd "$WORKSCRIPT" || exit
+}
+
+##
+## Crea un comando para generar plantillas de archivos
+##
+generador_plantillas() {
+    enlazarHome '.local/bin/nuevo'
+}
+
+devicons_ls() {
+    descargarGIT 'devicons-ls' 'https://github.com/ryanoasis/devicons-shell.git' "$WORKSCRIPT/tmp/devicons-shell"
+
+    if [[ -f "$HOME/.local/bin/devicons-ls" ]]; then
+        rm -f "$HOME/.local/bin/devicons-ls"
+    fi
+
+    cp "$WORKSCRIPT/tmp/devicons-shell/devicons-ls" "$HOME/.local/bin/devicons-ls"
+}
 
 aplicaciones_usuarios() {
     echo -e "$VE Instalando Aplicaciones específicas para el usuario$RO $USER$CL"
-}
+    configurar_heroku
+    firefox_instalador
+    bashit_Instalador
+    ohmyzsh_Instalador
 
-###########################
-##       EJECUCIÓN       ##
-###########################
+    while true; do
+        echo -e "$VE ¿Quieres instalar$RO vim$VE o$RO spacevim$CL"
+        echo -e "$RO 1)$VE vim$CL"
+        echo -e "$RO 2)$VE spacevim$CL"
+        read -p "→ " editor
+        case "$editor" in
+            vim | 1)
+                vim_Instalador
+                break;;
+            spacevim | 2)
+                spacevim_Instalador
+                break;;
+            *)  ## Opción errónea
+                clear
+                echo -e "$RO Opción no válida$CL"
+        esac
+    done
+
+    devicons_ls
+
+    ## Mis propias aplicaciones
+    generador_proyectos
+    generador_plantillas
+}
